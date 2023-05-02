ae6f105967e8f74e8361bb1c9420ce7e286e8224
MattWalker1 <mwalkermain@gmail.com>
2023-05-01 20:42:45 -0600

    \_/

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
 
 
-//insert the ReactRouter wrapper around the app in here, remove strictmode. strictmode is for development builds only, once read to build to production, 
-  // then remove <React.Strictmode>
+//insert the ReactRouter wrapper around the app in here, remove strictmode. (strictmode=devmode)
+  
