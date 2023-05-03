10f91e754585dcbc264db25b7a413dc5c4887f42
MattWalker1 <mwalkermain@gmail.com>
2023-05-03 14:33:44 -0600

    Navigate

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Button = ({ styles }) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[14px] text-primary outline-none ${styles} rounded-[10px]`} onClick={() => window.location = 'mailto:mwalkermain@gmail.com'}>Contact us today</button>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[14px] text-primary outline-none ${styles} rounded-[10px]`} 
+    onClick={() => window.location = 'mailto:mwalkermain@gmail.com'}>Contact us today</button>
   )
 }
 
