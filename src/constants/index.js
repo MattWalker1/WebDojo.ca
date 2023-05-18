45ae5394cc2fc7de3d539c1803b5e9b1b6417e55
MattWalker1 <mwalkermain@gmail.com>
2023-05-17 21:30:16 -0600

    clnup

--- src/constants/index.js
+++ src/constants/index.js
@@ -140,21 +140,3 @@ export const socialMedia = [
   },
 ];
 
-export const clients = [
-  {
-    id: "client-1",
-    logo: airbnb,
-  },
-  {
-    id: "client-2",
-    logo: binance,
-  },
-  {
-    id: "client-3",
-    logo: coinbase,
-  },
-  {
-    id: "client-4",
-    logo: dropbox,
-  },
-];
