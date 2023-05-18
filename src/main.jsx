91b570fd7c57e28d17e59c6465422c3f80df8506
MattWalker1 <mwalkermain@gmail.com>
2023-05-17 22:19:10 -0600

    rebuilding production build for deployment to live master

--- src/main.jsx
+++ src/main.jsx
@@ -8,5 +8,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 )
 
 
-//insert the ReactRouter wrapper around the app in here, remove strictmode. (strictmode=devmode)
+//insert the ReactRouter wrapper around the app in here, remove strictmode.
   
