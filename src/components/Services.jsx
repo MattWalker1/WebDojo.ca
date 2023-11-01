6b8b5796d49d73fc6d3ec7502706acac206a74d0
MattWalker1 <mwalkermain@gmail.com>
2023-11-01 07:31:12 -0600

    services text edit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -24,9 +24,9 @@ const Services = () => {
     <section id="features" className={layout.section}>
     
         <div className={layout.sectionInfo}>
-          <h2 className={styles.heading2}> Modern website design,<br className="sm:block hidden"/> packed full of blazingly fast custom features.</h2>
-          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>We offer web development services in 3 simple packages. Gold, Silver & Bronze. 
-          Depending on what level of customizability and scalability your business needs.</p>
+          <h2 className={styles.heading2}> Modern website design,<br className="sm:block hidden"/> packed full of blazingly fast, custom features.</h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Web Dojo offers custom Web development in 3 simple packages. Gold, Silver & Bronze. 
+          Depending on what level of customizability and scalability your business needs. Each package contains the same expert level of design & customer care, so you don't have to compromise, no matter your budget..</p>
         </div>
 
         <div className={`${layout.sectionImg} flex-col`}>
